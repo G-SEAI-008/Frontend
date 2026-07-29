4770ed08dac57d9a01670e236bdec7cb92dd6ebb
Renke <renke.brixel@gmail.com>
2026-07-29 10:18:40 +0200

    ESModules

---
+++ 11_JavaScript_Modules/03-modules/anotherScript.js
@@ -0,0 +1,10 @@
+export function add(a, b) {
+  return a + b;
+}
+
+// * Daten in den LocalStorage schreiben
+export default function writeToStorage(key, data) {
+  console.log('Writing to LocalStorage');
+}
+
+export { TASK_STORAGE_KEY };
---
+++ 11_JavaScript_Modules/03-modules/src/config.js
@@ -0,0 +1,4 @@
+// * Zentraler Schlüssel für die Taskdaten
+const TASK_STORAGE_KEY = 'myTasks';
+
+export { TASK_STORAGE_KEY };
