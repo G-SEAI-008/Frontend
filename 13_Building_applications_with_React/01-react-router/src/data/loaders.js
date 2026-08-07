a59f5cb5e1469b4f7e340959cff655a917e4fa8b
Renke <renke.brixel@gmail.com>
2026-08-07 14:33:34 +0200

    add starter code for react-router

---
+++ 13_Building_applications_with_React/01-react-router/src/data/loaders.js
@@ -0,0 +1,12 @@
+// oxlint-disable promise/avoid-new
+export const starsLoader = async () => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+  const res = await fetch('/stars.json');
+  if (res.status !== 200) {
+    throw new Error('something went wrong');
+  }
+  const data = await res.json();
+  return data;
+};
