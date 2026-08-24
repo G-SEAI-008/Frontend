81bb71ab8b06e44d402f2b8b3c71ebdebf8657af
Renke Brixel <renke.brixel@gmail.com>
2026-08-24 17:29:49 +0200

    rename the directory

--- 15_TypeScript_in_React/02-typing-hooks/src/components/shared/Button.jsx
+++
@@ -1,4 +0,0 @@
-const Button = ({ className, children }) => {
-  return <button className={`btn btn-primary ${className}`}>{children}</button>;
-};
-export default Button;
--- 15_TypeScript_in_React/02-typing-hooks/src/contexts/ThemeContext.jsx
+++
@@ -1,8 +0,0 @@
-import { use } from 'react';
-import { createContext } from 'react';
-
-const ThemeContext = createContext('halloween');
-
-const useTheme = () => use(ThemeContext);
-
-export { ThemeContext, useTheme };
