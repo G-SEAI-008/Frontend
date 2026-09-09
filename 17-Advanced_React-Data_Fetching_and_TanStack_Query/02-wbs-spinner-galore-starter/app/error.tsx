254ff4f5425005e2ff7c647548212b3e375587f7
Renke Brixel <renke.brixel@gmail.com>
2026-09-09 14:52:45 +0200

    go back to old text

--- 17-Advanced_React-Data_Fetching_and_TanStack_Query/02-wbs-spinner-galore-starter/app/error.tsx
+++ 17-Advanced_React-Data_Fetching_and_TanStack_Query/02-wbs-spinner-galore-starter/app/error.tsx
@@ -4,7 +4,7 @@ const ErrorPage = ({ retry }: { retry: () => void }) => {
   return (
     <section role='alert'>
       <h1>Die Daten konnten nicht geladen werden.</h1>
-      <p>Bitte versuche es noch einmal.</p>
+      <p>Bitte überprüfe deine Internetverbindung und versuche es noch einmal.</p>
       <button onClick={retry}>Erneut versuchen</button>
     </section>
   );
